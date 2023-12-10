d32232d5144dfca314235812054c3b815b929553
Aron Teh <aronteh.chess@gmail.com>
2023-12-10 17:42:25 +0000

    Remove unnecessary boilerplate code

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
-import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+'use client';
+
+import { Button, Group, Title, useMantineColorScheme } from '@mantine/core';
+import classes from './AppPage.module.css';
 
 export default function HomePage() {
+  const { setColorScheme } = useMantineColorScheme();
+
   return (
     <>
-      <Welcome />
-      <ColorSchemeToggle />
+      <Title className={classes.title} ta="center" mt={100}>
+        Welcome
+      </Title>
+      <Group justify="center" mt="xl">
+        <Button onClick={() => setColorScheme('light')}>Light</Button>
+        <Button onClick={() => setColorScheme('dark')}>Dark</Button>
+        <Button onClick={() => setColorScheme('auto')}>Auto</Button>
+      </Group>
     </>
   );
 }
